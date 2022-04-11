604565a6840db395ddca7100a97c275cea50b007
Gonza57 <37488218+Gonza57@users.noreply.github.com>
2022-04-11 09:58:04 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,31 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: "mysql",
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi_bd'),
+        username: env('DATABASE_USERNAME', 'Strapiadmin'),
+        password: env('DATABASE_PASSWORD', 'Mayorista@2022'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
     },
-    useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//  connection: {
+//    client: 'sqlite',
+//    connection: {
+//      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//    },
+//    useNullAsDefault: true,
+//  },
+// });
